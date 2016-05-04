910de174c66bf0a179af3d62dde0dd3b810a71de
vinothkannans <vinothkannan@vinkas.com>
2016-05-04 19:10:42 +0530

    First commit
    Initial upload after project creation

---
+++ app/src/main/java/com/vinkas/reminders/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.vinkas.reminders;
+
+/**
+ * Created by Vinoth on 4-5-16.
+ */
+public class SplashActivity extends vinkas.io.app.SplashActivity {
+
+    @Override
+    protected Class<?> getMainActivityClass() {
+        return TestActivity.class;
+    }
+}
---
+++ app/src/main/java/com/vinkas/reminders/TestActivity.java
@@ -0,0 +1,9 @@
+package com.vinkas.reminders;
+
+import vinkas.io.app.Activity;
+
+/**
+ * Created by Vinoth on 4-5-16.
+ */
+public class TestActivity extends Activity {
+}
